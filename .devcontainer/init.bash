0414b8533566ca9db24de57837e6526c1ef3db5d
TAKE Takashi <nakamizo.takashi@gmail.com>
2023-02-09 15:00:27 +0000

    try install ssm

--- .devcontainer/init.bash
+++ .devcontainer/init.bash
@@ -3,3 +3,8 @@
 npm i
 cd cdk
 npm i
+
+# install session-manager-plugin
+cd /tmp
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+dpkg -i session-manager-plugin.deb
