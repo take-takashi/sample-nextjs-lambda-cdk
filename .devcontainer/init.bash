ddcf86bdb195a231635e25d78865ee61ba1de5eb
TAKE Takashi <nakamizo.takashi@gmail.com>
2023-02-10 14:48:57 +0000

    devcontainer.jsonを推奨設定に修正

--- .devcontainer/init.bash
+++ .devcontainer/init.bash
@@ -7,4 +7,4 @@ npm i
 # install session-manager-plugin
 cd /tmp
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
-dpkg -i session-manager-plugin.deb
+sudo dpkg -i session-manager-plugin.deb
