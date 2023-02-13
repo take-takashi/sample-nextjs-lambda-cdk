85816eeb60e5d85697c57d134c1f442182ed3802
TAKE Takashi <nakamizo.takashi@gmail.com>
2023-02-13 16:35:44 +0000

    試しに.env.localを自動生成するようにした

--- .devcontainer/init.bash
+++ .devcontainer/init.bash
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+WORKSPACE=$PWD
+
 npm i
 cd cdk
 npm i
+
+# CodespacesのSecretから環境変数展開して.bashrcに記載
+#cd ~
+#echo "export AWS_ACCESS_KEY_ID=${AWS_AWS_ACCESS_KEY_ID}" >> .bashrc
+#echo "export AWS_SECRET_ACCESS_KEY=${AWS_AWS_SECRET_ACCESS_KEY}" >> .bashrc
+#echo "export AWS_REGION=${AWS_AWS_REGION}" >> .bashrc
+
+# CodespacesのSecretから環境変数展開して.env.localに記載
+cd $WORKSPACE
+echo "" > .env.local
+echo "DB_HOST=${PRD_DB_HOST}" >> .env.local
+echo "DB_USER=${PRD_DB_USER}" >> .env.local
+echo "DB_PASS=${PRD_DB_PASS}" >> .env.local
 
 # install session-manager-plugin
 cd /tmp
